379706f39536b2572a7c222fbb3d7ef6d9c23d21
Toby Liu <ybot1122@gmail.com>
2018-06-28 21:46:57 -0700

    support redux thunk

--- store/store.js
+++ store/store.js
@@ -1,22 +1,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk';
 import bootstrap from '../reducers/bootstrap'
 
-const reducers = combineReducers({
+const rootReducer = combineReducers({
   bootstrap,
 })
 
-const initialState = {
-  boostrap: {
-    todos: [],
-  },
-}
-
-// not used yet
-const enhancer = compose(
-  applyMiddleware(),
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
 )
-
-const store = createStore(reducers, [initialState])
 
 console.log('Store Initialized: ', store.getState())
 
